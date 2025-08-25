61f14b399e2bd59f4c010f075323d0fc2906e193
Addemark <addemark@gmail.com>
2025-08-25 22:12:40 +0100

    add port number to express

--- server-dev.js
+++ server-dev.js
@@ -1,32 +1,36 @@
-import fs from 'fs';
-import express from 'express';
-import { createServer } from 'vite';
+import fs from "fs";
+import express from "express";
+import { createServer } from "vite";
 
 const app = express();
+const port = process.env.PORT || 3030;
 
 const vite = await createServer({
   server: {
     middlewareMode: true,
   },
-  appType: 'custom',
+  appType: "custom",
 });
 
 app.use(vite.middlewares);
 
-app.use('*', async (req, res) => {
+app.use("*", async (req, res) => {
   const url = req.originalUrl;
 
   try {
-    const template = await vite.transformIndexHtml(url, fs.readFileSync('index.html', 'utf-8'));
-    const { render } = await vite.ssrLoadModule('/src/entry-server.jsx');
+    const template = await vite.transformIndexHtml(
+      url,
+      fs.readFileSync("index.html", "utf-8")
+    );
+    const { render } = await vite.ssrLoadModule("/src/entry-server.jsx");
 
     const html = template.replace(`<!--outlet-->`, render);
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    res.status(200).set({ "Content-Type": "text/html" }).end(html);
   } catch (error) {
     res.status(500).end(error);
   }
 });
 
-app.listen(4173, () => {
-  console.log('http://localhost:4173.');
+app.listen(port, () => {
+  console.log(`http://localhost:${port}.`);
 });
--- server-prod.js
+++ server-prod.js
@@ -1,24 +1,30 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3030;
 
-app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client'), { index: false }));
+app.use(
+  express.static(
+    path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist/client"),
+    { index: false }
+  )
+);
 
-app.use('*', async (_, res) => {
+app.use("*", async (_, res) => {
   try {
-    const template = fs.readFileSync('./dist/client/index.html', 'utf-8');
-    const { render } = await import('./dist/server/entry-server.js');
+    const template = fs.readFileSync("./dist/client/index.html", "utf-8");
+    const { render } = await import("./dist/server/entry-server.js");
 
     const html = template.replace(`<!--outlet-->`, render);
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    res.status(200).set({ "Content-Type": "text/html" }).end(html);
   } catch (error) {
     res.status(500).end(error);
   }
 });
 
-app.listen(5173, () => {
-  console.log('http://localhost:5173.');
+app.listen(port, () => {
+  console.log(`http://localhost:${port}.`);
 });
